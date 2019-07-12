f19fd89526e3ff3a29296c47eb0dd9484a5e7b62
MingJia <cbren@genedenovo.com>
2019-07-12 11:14:56 +0800

    feat(new): add density plot demo

--- README.md
+++ README.md
@@ -4,4 +4,13 @@
 ## Bar Plot
 
 ## Density Plot
+![Density Plot](density/density.png)
+
+```python
+python density.py -i density.data -n mRNA,lncRNA -p density -y Density -x "Median log10(FPKM)" -t "Density Plot" -c "black,red"
+```
+
+[脚本路径](density/density.py)
+
+[数据路径](density/density.data)
 
--- density/density.py
+++ density/density.py
@@ -3,12 +3,13 @@
 # @Author: Ming
 # @Date:   2019-07-11 23:57:09
 # @Last Modified by:   Ming
-# @Last Modified time: 2019-07-11 23:57:09
+# @Last Modified time: 2019-07-12 10:53:46
 import matplotlib as mpl
 
 mpl.use('Agg')
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import click
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
@@ -20,19 +21,58 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
               required=True,
               type=click.Path(),
               help="The table input to plot.")
+@click.option('-n', '--names',
+              required=True,
+              help="The names group you want to plot, sep by ,")
 @click.option('-p', '--prefix',
               default='result',
               help="The out prefix.")
+@click.option('-x', '--xlab',
+              required=False,
+              help="The xlab for the plot")
+@click.option('-y', '--ylab',
+              required=False,
+              help="The ylab for the plot.")
 @click.option('-t', '--title',
               required=False,
               help="The title for the plot.")
-@click.option('-c', '--color',
+@click.option('-c', '--colors',
               required=False,
-              help="The colors used for plot, html code split by \,")
-def cli(input, prefix, title, color):
+              help="The colors used for plot, html code split by ,")
+def cli(input, names, prefix, xlab, ylab, title, colors):
     """
+    Density plot with python.
     """
-    df = pd.read_table(input, header=T)
+    df = pd.read_csv(input, header=0, names=None, sep='\t')
+
+    names = names.strip().split(',')
+    colors = colors.strip().split(',') if colors else None
+    if colors and (len(names) != len(colors)):
+        raise ValueError('Diff name and color number!')
+    # draw
+    plt.figure(figsize=(16, 10), dpi=300)
+    figure, ax = plt.subplots(figsize=(16, 8), dpi=300)
+
+    for i in range(len(names)):
+        if colors:
+            color = colors[i]
+            ax = sns.kdeplot(df[df.iloc[:, 0] == names[i]].iloc[:, 1],
+                             shade=False,
+                             color=color, label=names[i], alpha=.7)
+        else:
+            ax = sns.kdeplot(df[df.iloc[:, 0] == names[i]].iloc[:, 1],
+                             shade=False,
+                             label=names[i], alpha=.7)
+
+    if xlab:
+        ax.set_xlabel(xlab, fontsize=15)
+    if ylab:
+        ax.set_ylabel(ylab, fontsize=15)
+    if title:
+        ax.set_title(title, fontsize=22)
+
+    plt.savefig(prefix + '.svg', bbox_inches='tight')
+    plt.savefig(prefix + '.png', dpi=300, bbox_inches='tight')
 
 
 if __name__ == "__main__":
