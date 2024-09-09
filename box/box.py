2bb4fb76f4ec1ed4523ae591feba567315720cd2
Chaobo Ren <xinbada426@gmail.com>
2024-09-09 11:26:06 +0800

    Update(heatmap): 添加一些命令行参数，修改默认调色板，修改默认样式

--- box/box.py
+++ box/box.py
@@ -4,6 +4,7 @@
 # @Date:   2019-07-12 16:01:58
 # @Last Modified by:   MingJia
 # @Last Modified time: 2019-10-16 10:54:17
+import logging
 import matplotlib as mpl
 
 mpl.use('Agg')
@@ -11,6 +12,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import click
+
+logger = logging.getLogger(__file__)
+logger.addHandler(logging.NullHandler())
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
@@ -57,11 +64,22 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.option('-p', '--prefix',
               default='result',
               help="The out prefix.")
+@click.option('--width',
+              default=8,
+              show_default=True,
+              type=float,
+              help="The width of the plot.")
+@click.option('--height',
+              default=6,
+              show_default=True,
+              type=float,
+              help="The height of the plot.")
 def cli(input, xname, yname, huename, xorder, hueorder, huecolors, ylab, title,
-        xrotation, showfliers, prefix):
+        xrotation, showfliers, prefix, width, height):
     """
     Box plot with python.
     """
+    logger.info(f"Input: {input}")
     df = pd.read_csv(input, sep='\t')
 
     x_order = xorder.strip().split(',') if xorder else None
@@ -69,16 +87,35 @@ def cli(input, xname, yname, huename, xorder, hueorder, huecolors, ylab, title,
     hue_color = huecolors.strip().split(',') if huecolors else None
 
     # Draw
-    figure, axis = plt.subplots(figsize=(16, 8), dpi=300)
+    logger.info("Start to draw the box plot.")
+    if not huecolors:
+        sns.set_palette("Set1")
+    figure, axis = plt.subplots(figsize=(width, height))
     sns.boxplot(data=df, x=xname, y=yname, hue=huename, order=x_order,
                 showfliers=showfliers, hue_order=hue_order, color=hue_color,
                 width=0.6, ax=axis)
 
+    # 去除图例边框
+    handles, labels = axis.get_legend_handles_labels()  # 获取图例句柄和标签（如果需要的话）
+    legend = axis.legend(handles, labels,
+                         frameon=False,
+                         loc='center left',
+                         bbox_to_anchor=(1, 0.5),
+                         borderaxespad=0.)  # 创建一个新的图例并关闭边界框
+
+    if hasattr(axis, 'legend_') and axis.legend_ is not None:
+        axis.legend_.set_title('')
+
+    # 隐藏上面和右面的边框
+    axis.spines['top'].set_visible(False)
+    axis.spines['right'].set_visible(False)
+
+    axis.set_xticks(axis.get_xticks())
     axis.set_xticklabels(axis.get_xticklabels(), rotation=xrotation)
     axis.set(xlabel="", ylabel=ylab)
     axis.set_title(title, fontdict={'size': 22})
 
-    plt.savefig(prefix + '.svg', bbox_inches='tight')
+    plt.savefig(prefix + '.pdf', bbox_inches='tight')
     plt.savefig(prefix + '.png', dpi=300, bbox_inches='tight')
 
 
